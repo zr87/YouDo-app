6be7accfdec99370109c82f0a33f2f5852bc19a4
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2021-02-07 23:13:33 +0100

    Added MZ-04,05 story points
    #f irst run code, needs refactor!

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const listEl = document.getElementById("todo-list");
     const submitBtn = formEl.submit;
 
+    //disable submit by default
+    submitBtn.disabled = true;
+
     // add task on form submit
     formEl.addEventListener("submit", event => {
         //prevent browser refresh
@@ -35,6 +38,28 @@ document.addEventListener('DOMContentLoaded', () => {
         // if user hit OK then remove LI item from the UL list!
         if (shuldBeDeleted) {
             event.target.parentElement.remove();
+        }
+    });
+
+    //adding validation on keyup
+    formEl.todo.addEventListener("keyup", event => {
+        //stooping event bubbling
+        //event.stopPropagation();
+
+        console.log("value", event.target.value);
+
+        if(event.target.value.length >= 6) {
+            event.target.classList.add("valid");
+            event.target.classList.remove("invalid");
+            submitBtn.disabled = false;
+        } else if (event.target.value === "") {
+            event.target.classList.remove("valid");
+            event.target.classList.remove("invalid");
+            submitBtn.disabled = true;
+        } else {
+            event.target.classList.remove("valid");
+            event.target.classList.add("invalid");
+            submitBtn.disabled = true;
         }
     });
 

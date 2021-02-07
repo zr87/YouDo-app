5b0e5c517ce559628c0d45bb120e4f0d3d894636
Zoltan Rakottyai <zoltanrakottyai@gmail.com>
2021-02-07 23:04:09 +0100

    MZ-01,02,03 -implement first 3 story points
    #first run code, needs refactor!

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,41 @@
-//can be deleted
-console.log("Console log works!");
+
+document.addEventListener('DOMContentLoaded', () => {
+    const formEl = document.getElementById("form");
+    const listEl = document.getElementById("todo-list");
+    const submitBtn = formEl.submit;
+
+    // add task on form submit
+    formEl.addEventListener("submit", event => {
+        //prevent browser refresh
+        event.preventDefault();
+        // saving the inputs value
+        const taskText = formEl.todo.value;
+        //create list item element
+        const listItemEl = document.createElement("li");
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("button,delete");
+        deleteBtn.innerText = "delete";
+        // set  the elements inner text
+        listItemEl.textContent = taskText;
+        listItemEl.append(deleteBtn);
+        // append the created li element
+        listEl.append(listItemEl);
+        //clearing the input
+        formEl.todo.value = null;
+    });
+
+    // add event listener on the UL list to catch delete btn clicks
+    listEl.addEventListener("click", event => {
+        //stooping event bubbling
+        event.stopPropagation();
+
+        //prompt user to confirm delete
+        const shuldBeDeleted = window.confirm("Do you really want to delete this task?");
+
+        // if user hit OK then remove LI item from the UL list!
+        if (shuldBeDeleted) {
+            event.target.parentElement.remove();
+        }
+    });
+
+});
